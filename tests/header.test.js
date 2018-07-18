05bd46203f1cc249b12ea85a58d67a2aded1db6b
Abhinav <hemrajjain1999@gmail.com>
2018-07-18 22:55:32 +0530

    Made CustomPage class Proxy to Page class in puppetter

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,34 +1,31 @@
-const puppeteer = require("puppeteer");
-let browser, page;
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
+const Page = require("./helpers/page");
+
+let page;
+
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto("localhost:3000");
 });
+
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
+
 test("Header has a correct test", async () => {
   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
   expect(text).toEqual("Blogster");
 });
+
 test("Clicking login starts oauth flow", async () => {
   await page.click(".right a");
 
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
 test("When signed in, shows logout button", async () => {
-  const user = await userFactory();
-  const { session, sig } = sessionFactory(user);
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
+
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   expect(text).toEqual("Logout");
 });
